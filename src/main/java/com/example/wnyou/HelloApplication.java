ad0eb0e23058271ca31cbac740fc311f0e66703b
tonisann <haraanton.s@gmail.com>
2022-11-23 16:00:26 +0200

    Initial commit 23.11.2022 0.3

--- src/main/java/com/example/wnyou/HelloApplication.java
+++ src/main/java/com/example/wnyou/HelloApplication.java
@@ -2,8 +2,10 @@ package com.example.wnyou;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
@@ -11,10 +13,14 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
 
+        //new
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 150, Color.BLUE);
         stage.setTitle("WN You");
         stage.setScene(scene);
         stage.show();
